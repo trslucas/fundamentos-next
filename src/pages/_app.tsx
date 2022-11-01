3dd7f95ecb6c6f749034f5e0902fd622a64859a8
Lucas Trindade <trslucas@outlook.com>
2022-11-01 13:24:55 -0300

    Trabalhando com imagens

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
-
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from "../styles/pages/app";
+import Image from "next/legacy/image";
 
 globalStyles();
 
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
