25323149815f4ada3a7a222dcfc6212251ec9be0
Lucas Trindade <trslucas@outlook.com>
2022-11-27 15:39:43 -0300

    Estruturando página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,34 @@
+import { GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
 
     const { query } = useRouter()
     return (
-        <h1>Product {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,00</span>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi obcaecati accusamus neque voluptas explicabo itaque eius! Iure nobis assumenda fugiat veritatis voluptas illo ea exercitationem. Maxime eos mollitia quod consequuntur.</p>
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    
+    return {
+        props: {
+
+        },
+        revalidate: 60 * 60 * 1
+    }
+
 }
