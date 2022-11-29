f405a34085c3254b748085dada8530c1280bf8df
Lucas Trindade <trslucas@outlook.com>
2022-11-29 20:38:26 -0300

    Estrutura página de sucesso na compra

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,23 @@
+import Image from "next/legacy/image";
+import Link from "next/link";
+import { ImageContainer } from "../styles/pages/product";
+import { SuccessContainer } from "../styles/pages/sucess";
+
 export default function Success() {
     return (
-        <h1>Success!</h1>
+        <SuccessContainer>
+            <h1>Compra Efetuada</h1>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Uhuul <strong>Lucas Trindade</strong>, sua <strong>Camiseta Beyound the Limits</strong> já está à caminho da sua casa.
+            </p>
+
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+        </SuccessContainer>
     )
 }
